f3ba59cd82ab50f994cdc73add7466a4b54bd13c
vebjomy <vebjomy@stud.ntnu.no>
2025-11-20 13:25:45 +0100

    Added javadoc comment.

--- Client/src/main/java/app/ShadeMain.java
+++ Client/src/main/java/app/ShadeMain.java
@@ -5,6 +5,12 @@ package app;
  * main class which does <i>not</i> extend Application is needed.
  */
 public class ShadeMain {
+
+  /**
+   * The main method which launches the JavaFX application.
+   *
+   * @param args the command line arguments
+   */
   public static void main(String[] args) {
     MainApp.main(args);
   }
