9769e3d24ccf137576030398cf8fece813e5cef7
DymTho <dymitrid@stud.ntnu.no>
2025-11-20 14:37:38 +0100

    Removed unused classes, added JavaDoc to ErrorMsg.java

--- protocol/src/main/java/dto/ErrorMsg.java
+++ protocol/src/main/java/dto/ErrorMsg.java
@@ -1,5 +1,9 @@
 package dto;
 
+/**
+ * Represents an error message in the protocol.
+ * Contains the error type, request ID, error code, and descriptive message.
+ */
 public class ErrorMsg {
   public String type = "error";
   public String id;
